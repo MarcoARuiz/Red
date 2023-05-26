eb19571932eae5d9d5f5bf28cb8fc8e565724725
Marco <marcoantonio.ruizgu@gmail.com>
2023-05-25 21:44:59 -0600

    done

--- RedNN.py
+++ RedNN.py
@@ -8,18 +8,18 @@ from loss import mse, mse_prime
 
 # training data
 x_train = np.array([[[0,0,0,0,0,0,0,0,0,0]], [[0,0,0,0,0,1,1,1,1,1]], [[1,1,1,1,1,0,0,0,0,0]], [[1,1,1,1,1,1,1,1,1,1]]])
-y_train = np.array([[[0]], [[1]], [[1]], [[0]]])
+y_train = np.array([[[0]], [[1]], [[1]], [[2]]])
 
 # network
 net = Network()
 net.add(FCLayer(10, 4))
-net.add(ActivationLayer(tanh, tanh_prime))
+# net.add(ActivationLayer(tanh, tanh_prime))
 net.add(FCLayer(4, 2))
-net.add(ActivationLayer(tanh, tanh_prime))
+# net.add(ActivationLayer(tanh, tanh_prime))
 net.add(FCLayer(2, 3))
-net.add(ActivationLayer(tanh, tanh_prime))
+# net.add(ActivationLayer(tanh, tanh_prime))
 net.add(FCLayer(3, 1))
-net.add(ActivationLayer(tanh, tanh_prime))
+# net.add(ActivationLayer(tanh, tanh_prime))
 
 # train
 net.use(mse, mse_prime)
